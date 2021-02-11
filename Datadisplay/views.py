654ac6a0e44043023cfb1ed11853eabf4ef218c4
Charlie Rettig <charlie__r@berkeley.edu>
2021-02-11 10:53:51 -0800

    added annotations

--- Datadisplay/views.py
+++ Datadisplay/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 import requests
 
-# Create your views here.
+# This file contains view methods that are responsible for rendering the HTML templates. 
+# The table_view renders the data table used on the main page of the website. 
+# The blank_view was used for testing the 'Fetch Data' button.
 def table_view(request):
 	response = requests.get('https://api.usaspending.gov/api/v2/references/toptier_agencies/').json()["results"]
 	key_headers = response[0].keys()
